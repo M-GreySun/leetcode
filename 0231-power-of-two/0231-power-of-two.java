1cb090b997866900d3a59889242651fdf001537d
Marcie Grayson <112010448+M-GreySun@users.noreply.github.com>
2025-08-09 18:07:43 -0400

    Time: 1 ms (35.76%), Space: 41.1 MB (36.28%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         String bin= Integer.toBinaryString(n);
-        System.out.println(bin);
         int count=0;
         for(int i=0; i< bin.length(); i++){
             count= bin.charAt(i)== '1'? count+1: count;
