6fc4e3c08fa347b668d04dc8784af391301e1535
Marcie Grayson <112010448+M-GreySun@users.noreply.github.com>
2025-08-08 23:35:49 -0400

    Time: 0 ms (100%), Space: 71.9 MB (86.88%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.java
@@ -1,19 +1,5 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        int [] edgeFrequency = new int [edges.length +1];
-
-        for(int []ele: edges){
-
-            edgeFrequency[ele[0]-1]++;
-            edgeFrequency[ele[1]-1]++;
-            if(edgeFrequency[ele[0]-1]==edges.length){
-                return ele[0];
-            }
-            if(edgeFrequency[ele[1]-1]==edges.length){
-                return ele[1];
-            }
-        }        
-
-        return -1;
+        return edges[0][0]== edges[1][0] || edges[0][0]== edges[1][1]? edges[0][0]: edges[0][1];  
     }
 }
