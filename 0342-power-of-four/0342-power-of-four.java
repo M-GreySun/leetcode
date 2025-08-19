974787411021a1d0f9fe22221022b8def6a59e24
Marcie Grayson <112010448+M-GreySun@users.noreply.github.com>
2025-08-19 00:20:18 -0400

    Time: 0 ms (100%), Space: 40.6 MB (91.15%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1){
+            return true;
+        }
+    
+        return n==0 || n%4!=0? false: isPowerOfFour(n/4);
+    }
+}
