6a40191c2b11b9b36b51adb103c8df7dc5183333
Marcie Grayson <112010448+M-GreySun@users.noreply.github.com>
2025-08-19 00:25:03 -0400

    Time: 0 ms (100%), Space: 41.2 MB (16.35%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -3,7 +3,6 @@ class Solution {
         if(n==1){
             return true;
         }
-    
-        return n==0 || n%4!=0? false: isPowerOfFour(n/4);
+        return n<=0 || n%4!=0? false: isPowerOfFour(n/4);
     }
 }
