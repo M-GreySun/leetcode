a59f68a38383155b1bcbbcddef505cd97122667b
Marcie Grayson <112010448+M-GreySun@users.noreply.github.com>
2025-08-23 19:14:02 -0400

    Time: 8 ms (81.92%), Space: 44.1 MB (67.97%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if((n<=0 || n%3!=0) && n!=1){
+        if(n==1){
+            return true;
+        }
+        if(n<=0 || n%3!=0){
             return false;
         }
-        return n==1? true: isPowerOfThree(n/3);
-        
+        return isPowerOfThree(n/3);
     }
 }
