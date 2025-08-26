69e4836cfb4405a9f03eefd35418ddc3776cd576
Marcie Grayson <112010448+M-GreySun@users.noreply.github.com>
2025-08-25 20:35:07 -0400

    Time: 3 ms (61.49%), Space: 47 MB (82.45%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -5,42 +5,33 @@ class Solution {
         int row=0;
         int col=0;
         for (int index=0; index< ans.length; index++){
-            //System.out.println("The index: "+ index + "\tThe value added: "+ mat[row][col]);
             ans[index]=mat[row][col];
             
             if(up && row==0 && col<mat[row].length-1){
-                //System.out.println("First if");
                 col++;
                 up=false;
             }
             else if (up && row>0 && col<mat[row].length-1){
-                //System.out.println("Second if");
                 row--;
                 col++;
             }
             else if(up && col==mat[row].length-1){
-                //System.out.println("Third if");
                 row++;
                 up=false;
             }
             else if(!up && row<mat.length-1 && col>0){
-                //System.out.println("Fourth if");
                 row++;
                 col--;
             }
             else if(!up &&  row<mat.length-1 && col==0){
-                //System.out.println("Fifth if");
                 row++;
                 up=true;
             }
-            else if(!up && row==mat.length-1 && col<mat[row].length-1){
-                //System.out.println("Sixth if");
+            else if(!up && row==mat.length-1){
                 col++;
                 up=true;
             }
         }
-
-        System.out.println("\n"+ Arrays.toString(ans));
 
         return ans;        
     }
