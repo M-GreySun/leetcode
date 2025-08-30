fca45fbc7010b025571798edd91394f92d1af487
Marcie Grayson <112010448+M-GreySun@users.noreply.github.com>
2025-08-30 16:00:06 -0400

    Time: 1 ms (99.83%), Space: 46.3 MB (35.68%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans=nums[0];
-
-        for (int i=1; i< nums.length; i++){
-            ans= ans ^ nums[i];
+        int ans=0;
+        for (int ele:  nums){
+            ans^= ele;
         }
-    
         return ans;
     }
 }
