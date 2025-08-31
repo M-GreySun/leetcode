d13983a8419dab690eb87562bbb1abc187c305f1
Marcie Grayson <112010448+M-GreySun@users.noreply.github.com>
2025-08-31 16:08:14 -0400

    Time: 8 ms (86.05%), Space: 55 MB (75.82%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int [] basketOne= new int [3];
+        basketOne[0]=-1;
+    
+        int [] basketTwo =new int [3];
+        basketTwo[0]=-1;
+        int max=0;
+    
+        for(int i=0; i< fruits.length; i++){
+            if(basketOne[0]==-1){
+                basketOne[0]=fruits[i];
+                basketOne[1]=1;
+            }
+            else if (basketTwo[0]==-1 && basketOne[0]!=-1 && basketOne[0]!=fruits[i]){
+                basketTwo[0]=fruits[i];
+                basketTwo[1]=1;
+                basketOne[2]=basketOne[1];
+            }
+            else if(basketOne[0]==fruits[i]){
+                basketOne[1]+=1;
+                basketTwo[2]=basketTwo[1];
+            }
+            else if(basketTwo[0]==fruits[i]){
+                basketTwo[1]+=1;
+                basketOne[2]=basketOne[1];
+            }
+            else if(basketOne[0]!=fruits[i] && basketTwo[0]!=fruits[i]){
+                max=Math.max(max, basketOne[1]+ basketTwo[1]);
+
+                if(fruits[i-1]==basketOne[0]){
+                    basketOne[1]-=basketOne[2];
+                    basketOne[2]=basketOne[1];
+
+                    basketTwo[0]=fruits[i];
+                    basketTwo[1]=1;
+                    basketTwo[2]=0;
+                }
+                else{
+                    basketTwo[1]-=basketTwo[2];
+                    basketTwo[2]=basketTwo[1];
+
+                    basketOne[0]=fruits[i];
+                    basketOne[1]=1;
+                    basketOne[2]=0;
+                }
+            }
+            //printValues(basketOne, basketTwo);
+        }
+
+        return Math.max(max, basketOne[1]+ basketTwo[1]);        
+    }
+
+    public void printValues(int []one, int []two){
+        System.out.println(Arrays.toString(one));
+        System.out.println(Arrays.toString(two));
+        System.out.println("\n");
+    }
+}
