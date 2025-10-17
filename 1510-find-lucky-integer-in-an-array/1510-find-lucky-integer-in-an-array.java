95e349036330833ff739a6048141781979b23751
Marcie Grayson <112010448+M-GreySun@users.noreply.github.com>
2025-10-16 20:06:22 -0400

    Time: 15 ms (11.84%), Space: 44 MB (15.03%) - LeetHub

---
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        int ans=-1;
+        HashMap <Integer, Integer> freq= new HashMap <Integer, Integer> ();
+
+        for(int ele: arr){
+            if(freq.containsKey(ele)){
+                freq.put(ele, freq.get(ele)+1);
+            }
+            else{
+                freq.put(ele, 1);
+            }
+        }
+        System.out.println(freq);
+        for(Map.Entry <Integer, Integer> ele: freq.entrySet()){
+            if(ele.getKey().equals(ele.getValue()) && ele.getKey()>ans){
+                ans=ele.getKey();
+            }
+            System.out.println("Inside the bottom for loop");
+        }
+        return ans;
+    }
+}
