e15e5183f177b02c16b31286fb4a039740cd88e5
Marcie Grayson <112010448+M-GreySun@users.noreply.github.com>
2025-11-13 22:58:42 -0500

    Time: 0 ms (100%), Space: 41.8 MB (10.27%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.java
@@ -1,17 +1,5 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-
-        String val= Integer.toString(n, n-2);
-        System.out.println(val);
-
-        if(val.equals("12") || val.equals(String.valueOf(n))){
-            return false;
-        }
-        for(int i=0; i<val.length()/2; i++){
-            if(val.charAt(i)!=val.charAt(val.length()-i-1)){
-                return false;
-            }
-        }
-        return true;   
+        return false;
     }
 }
