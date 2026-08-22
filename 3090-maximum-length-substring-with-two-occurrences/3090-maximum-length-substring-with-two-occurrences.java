503c58ee61b84a0a0548cdf8ca9b3ff9c7026f30
Marcie Grayson <112010448+M-GreySun@users.noreply.github.com>
2026-08-22 15:52:56 -0400

    Time: 2 ms (44.75%), Space: 43.6 MB (50.07%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -6,8 +6,6 @@ class Solution {
         for (int i=0; i< s.length(); i++){
             letters[s.charAt(i)- 97]++;
             for(int j=i+1; j<s.length(); j++){
-                //System.out.println("The value of I: "+ i);
-                //System.out.println("The value of J: " + j+ "\n");
                 letters[s.charAt(j)-97]++;
                 if(letters[s.charAt(j)-97]> 2){
                     ans= Math.max(ans, j-i);
@@ -15,7 +13,6 @@ class Solution {
                 }
                 ans= Math.max(ans, j-i+1);
             }
-        //System.out.println(Arrays.toString(letters));
             for(int k=0; k<letters.length; k++){
                 letters[k]= 0;
             }
