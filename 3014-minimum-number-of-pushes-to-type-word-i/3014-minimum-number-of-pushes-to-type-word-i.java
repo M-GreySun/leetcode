30047d7d7a0446173a93f56f611bf2a8b10f083d
Marcie Grayson <112010448+M-GreySun@users.noreply.github.com>
2026-08-22 17:17:16 -0400

    Time: 0 ms (100%), Space: 43.2 MB (40.63%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -2,19 +2,9 @@ class Solution {
     public int minimumPushes(String word) {
         int ans=0;
         for(int i= 0; i< word.length(); i++){
-            if(i<8){
-                ans++;
-            }
-            else if(i<16){
-                ans+=2;
-            }
-            else if (i<24){
-                ans+=3;
-            }
-            else{
-                ans+=4;
-            }
+            ans+= i/8 + 1;
         }
+
         return ans;
     }
 }
